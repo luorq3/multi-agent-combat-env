a1736afc767bdd4947bc6e27c239f7375269bd32
ruiqingluo <ruiqingluo1@163.com>
2022-04-07 10:19:53 +0800

    # initial commit

--- multi_agent_combat_env/envs/sprites/base/__init__.py
+++
@@ -1,9 +0,0 @@
-from multi_agent_combat_env.envs.sprites.base.sprite_base import SpriteBase
-from multi_agent_combat_env.envs.sprites.base.movable import Movable
-from multi_agent_combat_env.envs.sprites.base.missile import Missile
-
-__all__ = [
-    SpriteBase.__name__,
-    Movable.__name__,
-    Missile.__name__
-]
